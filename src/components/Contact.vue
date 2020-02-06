615a59e988b9588b56b7666e70600e7ba6f73a4b
aniaska4 <aniaska4@wp.pl>
2020-02-06 14:28:44 +0100

    contact template

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,16 +3,12 @@
     .container
         .contact_box__title
             h1 Contact 
-        .contact_box__icons
-            .contact_box__icons--mail
-                img(src="./public/images/mail.png")
-            //- .contact_box__icons--github
-            //-     img(src="./images/github.png")
-            //-         a(href="https://github.com/aniaska4")
-            //- .contact_box__icons--linkedIn
-            //-     img(src="./images/linkedin.png")
-            //-         a(href="https://www.linkedin.com/in/anna-belka-71793649/")
-        
+        .contact_box_main
+            .contact_box__icons(v-for="icon in icons")
+                .contact_box__icons--item(:class="icon.class")
+                    img(:src="icon.img" target="blank" :alt="icon.alt" rel="nofollow" )
+                    a(:href="icon.href")
+            
 
 </template>
 
@@ -21,9 +17,9 @@ export default {
     data(){
         return {
             icons: [
-                {"class" : "contact_box__icons--mail", "img": "./public/images/mail.png"},
-                {"class" : "contact_box__icons--github", "img": "./public/images/mail.png"},
-                {"class" : "contact_box__icons--linkedIn", "img": "./public/images/mail.png"}
+                {"class" : "contact_box__icons--mail", "img": "images/mail.png", "href":"", "alt":"mail"},
+                {"class" : "contact_box__icons--github", "img": "images/github.png", "href":"https://github.com/aniaska4", "alt":"github"},
+                {"class" : "contact_box__icons--linkedIn", "img": "images/linkedin.png", "href":"https://www.linkedin.com/in/anna-belka-71793649/", "alt":"linkedin" }
             ]
         }
     }
@@ -42,6 +38,16 @@ export default {
     h1 
         font-size: 40px;
 
+.contact_box_main
+    display: flex;
+    justify: center;
+    align-items: center;
+    margin-top: 40px;
+    .contact_box__icons--item
+        padding: 40px;
+        img
+            width: 70%;  
+            cursor: pointer;  
 
 </style>
 
